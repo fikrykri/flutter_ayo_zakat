deb6ca5215a6e61daa8b8512d2fb0dca0069c39b
DESKTOP-T7BOLUN\ASUS <samsulnurhadiansah687@gmail.com>
2021-01-05 11:10:05 +0700

    menambahkan component string untuk page introduction

---
+++ lib/components/Strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static var stepOneTitle = "Selamat datang di Ayo Zakat";
+  static var stepOneContent = "Ayo Zakat memfasilitasi kalian untuk melakukan zakat secara lebih mudah";
+  static var stepTwoTitle = "AyoZakat";
+  static var stepTwoContent = "Berbagai macam zakat dapat dilakukan di sini";
+  static var stepThreeTitle = "AyoZakat";
+  static var stepThreeContent = "Bersama Ayo Zakat, Membuat zakat menjadi lebih mudah dan Menyenangkan";
+}
