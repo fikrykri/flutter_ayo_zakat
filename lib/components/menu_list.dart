8eeec19a5456f8835fd853a8b094976cbf42ebd3
FikryAS <azizfikry15@gmail.com>
2021-02-02 13:37:03 +0700

    update code

--- lib/components/menu_list.dart
+++ lib/components/menu_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ayo_zakat/dashboard';
+import 'package:flutter_ayo_zakat/dashboard.dart';
 
 class MenuList extends StatelessWidget {
   @override
@@ -11,105 +11,94 @@ class MenuList extends StatelessWidget {
         child: Align(
           alignment: Alignment.centerLeft,
           child: Column(
-            mainAxisSize:
-            MainAxisSize.min,
-            mainAxisAlignment:
-  MainAxisAlignment.spaceAround
+            mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Container(
                 width: 500,
                 height: 50,
                 child: RaisedButton(
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => 
-  DashboardPage()));
-            },
-            splashColor: Colors.yellowAccent,
-            color: Colors.grey[100],
-            elevation: 0,
-            child: Text("Dashboard",
-              style: TextStyle(color:
-  Colors.black,fontSize: 22)),
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DashboardPage()));
+                  },
+                  splashColor: Colors.yellowAccent,
+                  color: Colors.grey[100],
+                  elevation: 0,
+                  child: Text("Dashboard",
+                      style: TextStyle(color: Colors.black, fontSize: 22)),
+                ),
+              ),
+              Divider(
+                color: Colors.grey[200],
+              ),
+              Container(
+                width: 500,
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DashboardPage()));
+                  },
+                  splashColor: Colors.redAccent,
+                  color: Colors.grey[100],
+                  elevation: 0,
+                  child: Text("Profile",
+                      style: TextStyle(color: Colors.black, fontSize: 22)),
+                ),
+              ),
+              Divider(
+                color: Colors.grey[200],
+                height: 10,
+                thickness: 1,
+                indent: 120,
+              ),
+              Container(
+                width: 500,
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DashboardPage()));
+                  },
+                  splashColor: Colors.blueAccent,
+                  color: Colors.grey[100],
+                  elevation: 0,
+                  child: Text("Setting",
+                      style: TextStyle(color: Colors.black, fontSize: 22)),
+                ),
+              ),
+              Divider(
+                color: Colors.grey[200],
+                height: 10,
+                thickness: 1,
+                indent: 120,
+              ),
+              Container(
+                width: 500,
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {
+                    MaterialPageRoute(builder: (context) => DashboardPage());
+                  },
+                  splashColor: Colors.greenAccent,
+                  color: Colors.grey[100],
+                  elevation: 0,
+                  child: Text("About Us",
+                      style: TextStyle(color: Colors.black, fontSize: 22)),
+                ),
+              ),
+            ],
           ),
         ),
-        Divider(
-          color: Colors.grey[200] ,
-        )
-        Container(
-          width: 500,
-          height: 50,
-          child: RaisedButton(
-            onPressed: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-  DashboardPage()));
-            },
-            splashColor: Colors.redAccent,
-            color: Colors.grey[100],
-            elevation: 0,
-            child: Text("Profile",
-              style: TextStyle(color: 
-  Colors.black,fontSize: 22)),
-          ),
-        ),
-        Divider(
-          color: Colors.grey[200],
-          height: 10,
-          thickness: 1,
-          indent: 120,
-        ),
-        Container(
-          width: 500,
-          height: 50,
-          child: RaisedButton(
-            onPressed: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-  DashboardPage()));
-            },
-            splashColor: Colors.blueAccent,
-            color: Colors.grey[100],
-            elevation: 0,
-            child: Text("Setting",
-              style: TextStyle(color: 
-  Colors.black, fontSize: 22)),
-          ),
-        ),
-        Divider(
-          color: Colors.grey[200],
-          height: 10,
-          thickness: 1,
-          indent: 120,
-        ),
-        Container(
-          width: 500,
-          height: 50,
-          child: RaisedButton(
-            onPressed: (){
-              MaterialPageRoute(
-                builder: (context) =>
-  DashboardPage()));
-            },
-            splashColor: Colors.greenAccent,
-            color: Colors.grey[100],
-            elevation: 0,
-            child: Text("About Us"),
-              style: TextStyle(color:
-  Colors.black,fontSize: 22)),
-          ),
-      ],
-    ),
-  ),
-),
-);
+      ),
+    );
   }
 }
-
